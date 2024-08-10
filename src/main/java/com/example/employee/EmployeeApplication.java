891b7636e0638d1879838a55f903000695f65771
Dhevarajan_A <dhevarajan.alaguraju@netmeds.com>
2024-08-10 19:39:48 +0530

    renamed application name

--- src/main/java/com/example/employee/EmployeeApplication.java
+++ src/main/java/com/example/employee/EmployeeApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class EmployeeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(EmployeeApplication.class, args);
 	}
 
 }
